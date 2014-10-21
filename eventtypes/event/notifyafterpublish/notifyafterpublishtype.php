10af4eb6ec1a0b22a9c3f5cc1cb6eb119a7a18bb
Robert <robert@xrow.de>
2014-10-21 15:49:15 +0200

    - spaces removed

--- eventtypes/event/notifyafterpublish/notifyafterpublishtype.php
+++ eventtypes/event/notifyafterpublish/notifyafterpublishtype.php
@@ -12,73 +12,73 @@ class notifyAfterPublishType extends eZWorkflowEventType
     
     function execute( $process, $event )
     {
-		$parameters = $process->attribute( 'parameter_list' );
-		$xrowChangeMailINI = eZINI::instance( 'xrowmailchange.ini' );
-		$siteINI = eZINI::instance( 'site.ini' );
-		$http = eZHTTPTool::instance();
-		$exclude_siteaccesses = $xrowChangeMailINI->variable( 'GeneralSettings', 'WorkflowExcludeSiteaccess' );
-		$siteaccess = eZSiteAccess::current();
-		$tpl = eZTemplate::factory();
-		$cur_user = eZUser::currentUser();
-		$contentobject_id = $parameters["object_id"];
-		$user = eZuser::fetch($contentobject_id);
-		$old_mail = $user->attribute("email");
-		$new_mail = $http->postVariable('new_mail');
-		$exclude_usergroups = $xrowChangeMailINI->variable( 'GeneralSettings', 'UserGroupExcludes' );
-		$skip_workflow = false;
-		
-		foreach ( $exclude_usergroups as $usergroup_id )
-		{
-			if( in_array($usergroup_id, $cur_user->attribute("groups")) )
-			{
-				$skip_workflow = true;
-			}
-		}
-		
-		if( !in_array($siteaccess["name"], $exclude_siteaccesses) AND ($new_mail != "" && $new_mail !== $old_mail) AND $skip_workflow === false )
-		{
-			$db = eZDB::instance();
-			$receiver_type = $xrowChangeMailINI->variable( 'GeneralSettings', 'ConfirmationMailTo' );
-			$time = time();
-			$hash = md5( mt_rand() . $time . $contentobject_id );
-			$check_previous_request = $db->arrayQuery("SELECT * FROM xrow_mailchange WHERE user_id = $contentobject_id;");
-			if ( count($check_previous_request) >= 1 )
-			{
-				$db->begin();
-				$db->arrayQuery("DELETE FROM xrow_mailchange WHERE user_id = $contentobject_id;");
-				$db->commit();
-			}
-			$db->begin();
-			$db->arrayQuery("INSERT INTO xrow_mailchange ( hash, user_id, new_mail, change_time ) VALUES ( '$hash', $contentobject_id, '$new_mail', $time );");
-			$db->commit();
+        $parameters = $process->attribute( 'parameter_list' );
+        $xrowChangeMailINI = eZINI::instance( 'xrowmailchange.ini' );
+        $siteINI = eZINI::instance( 'site.ini' );
+        $http = eZHTTPTool::instance();
+        $exclude_siteaccesses = $xrowChangeMailINI->variable( 'GeneralSettings', 'WorkflowExcludeSiteaccess' );
+        $siteaccess = eZSiteAccess::current();
+        $tpl = eZTemplate::factory();
+        $cur_user = eZUser::currentUser();
+        $contentobject_id = $parameters["object_id"];
+        $user = eZuser::fetch($contentobject_id);
+        $old_mail = $user->attribute("email");
+        $new_mail = $http->postVariable('new_mail');
+        $exclude_usergroups = $xrowChangeMailINI->variable( 'GeneralSettings', 'UserGroupExcludes' );
+        $skip_workflow = false;
+        
+        foreach ( $exclude_usergroups as $usergroup_id )
+        {
+            if( in_array($usergroup_id, $cur_user->attribute("groups")) )
+            {
+                $skip_workflow = true;
+            }
+        }
+        
+        if( !in_array($siteaccess["name"], $exclude_siteaccesses) AND ($new_mail != "" && $new_mail !== $old_mail) AND $skip_workflow === false )
+        {
+            $db = eZDB::instance();
+            $receiver_type = $xrowChangeMailINI->variable( 'GeneralSettings', 'ConfirmationMailTo' );
+            $time = time();
+            $hash = md5( mt_rand() . $time . $contentobject_id );
+            $check_previous_request = $db->arrayQuery("SELECT * FROM xrow_mailchange WHERE user_id = $contentobject_id;");
+            if ( count($check_previous_request) >= 1 )
+            {
+                $db->begin();
+                $db->arrayQuery("DELETE FROM xrow_mailchange WHERE user_id = $contentobject_id;");
+                $db->commit();
+            }
+            $db->begin();
+            $db->arrayQuery("INSERT INTO xrow_mailchange ( hash, user_id, new_mail, change_time ) VALUES ( '$hash', $contentobject_id, '$new_mail', $time );");
+            $db->commit();
 
-			$tpl->setVariable( 'hostname', eZSys::hostname() );
-			$tpl->setVariable( 'hash', $hash );
-			$tpl->setVariable( 'new_mail', $new_mail );
-			$tpl->setVariable( 'old_mail', $old_mail );
+            $tpl->setVariable( 'hostname', eZSys::hostname() );
+            $tpl->setVariable( 'hash', $hash );
+            $tpl->setVariable( 'new_mail', $new_mail );
+            $tpl->setVariable( 'old_mail', $old_mail );
 
-			$templateResult = $tpl->fetch( 'design:mailchange/mail/activation.tpl' );
-	
-			$mail = new eZMail();
-			$mail->setSender( $siteINI->variable( 'MailSettings', 'EmailSender' ) );
-			if ( $receiver_type == "oldaddress")
-			{
-				$mail->setReceiver( $old_mail );
-			}
-			else
-			{
-				$mail->setReceiver( $new_mail );
-			}
-			$mail->setSubject( ezpI18n::tr( 'extension/xrowmailchange', 'Please approve you new email address' ) );
-			$mail->setBody( $templateResult );
-			$mailResult = eZMailTransport::send( $mail );
-		}
-		else
-		{
-			//fallback for cases where the workflow shouldnt run through the mail change process
-			$user->setAttribute( "email", "$new_mail" );
-			$user->store();
-		}
+            $templateResult = $tpl->fetch( 'design:mailchange/mail/activation.tpl' );
+    
+            $mail = new eZMail();
+            $mail->setSender( $siteINI->variable( 'MailSettings', 'EmailSender' ) );
+            if ( $receiver_type == "oldaddress")
+            {
+                $mail->setReceiver( $old_mail );
+            }
+            else
+            {
+                $mail->setReceiver( $new_mail );
+            }
+            $mail->setSubject( ezpI18n::tr( 'extension/xrowmailchange', 'Please approve you new email address' ) );
+            $mail->setBody( $templateResult );
+            $mailResult = eZMailTransport::send( $mail );
+        }
+        else
+        {
+            //fallback for cases where the workflow shouldnt run through the mail change process
+            $user->setAttribute( "email", "$new_mail" );
+            $user->store();
+        }
 
         return eZWorkflowType::STATUS_ACCEPTED;
     }
